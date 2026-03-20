8079d3bb39091c33118f41cfdb224fce070528cd
tiesen243 <ttien56906@gmail.com>
2026-03-20 20:58:43 +0700

    chore: minor changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import './app.css'
+
 import {
   Links,
   Meta,
@@ -5,17 +7,16 @@ import {
   Scripts,
   ScrollRestoration,
   isRouteErrorResponse,
-} from "react-router"
+} from 'react-router'
 
-import type { Route } from "./+types/root"
-import "./app.css"
+import type { Route } from './+types/root'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en' className='dark'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -33,15 +34,15 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!"
-  let details = "An unexpected error occurred."
+  let message = 'Oops!'
+  let details = 'An unexpected error occurred.'
   let stack: string | undefined
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error"
+    message = error.status === 404 ? '404' : 'Error'
     details =
       error.status === 404
-        ? "The requested page could not be found."
+        ? 'The requested page could not be found.'
         : error.statusText || details
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message
@@ -49,11 +50,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="container mx-auto p-4 pt-16">
+    <main className='container mx-auto p-4 pt-16'>
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full overflow-x-auto p-4">
+        <pre className='w-full overflow-x-auto p-4'>
           <code>{stack}</code>
         </pre>
       )}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,71 @@
-import { Button } from "~/components/ui/button"
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
+
+import { Button } from '~/components/ui/button'
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '~/components/ui/chart'
 
 export default function Home() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+    <div className='flex min-h-svh p-6'>
+      <div className='flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose'>
         <div>
-          <h1 className="font-medium">Project ready!</h1>
+          <h1 className='font-medium'>Project ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          <Button className='mt-2'>Button</Button>
         </div>
+
+        <Chart />
       </div>
+    </div>
+  )
+}
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: '#2563eb',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: '#60a5fa',
+  },
+}
+
+const Chart: React.FC = () => {
+  return (
+    <div className='mt-8 rounded-xl border p-4'>
+      <h2 className='mb-4 text-lg font-semibold'>Example Chart</h2>
+      <ChartContainer config={chartConfig} className='min-h-50 w-full'>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey='month'
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+          <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+        </BarChart>
+      </ChartContainer>
     </div>
   )
 }
